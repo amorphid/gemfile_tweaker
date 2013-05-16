59c1c580b48ca259c6ffb75f118ef4040367e678
Michael Pope <mpope.cr@gmail.com>
2013-05-15 19:05:47 -0700

    Create unit tests for parsing Gemfile

---
+++ spec/gemfile_tweaker_spec.rb
@@ -0,0 +1,23 @@
+require "gemfile_tweaker.rb"
+
+describe "Gemfile Tweaker" do
+	it "should contain Gemfile contents" do
+		pending
+	end
+
+	it "puts gem sources into an array" do
+		pending
+	end
+
+	it "puts all non-grouped gems into an array" do
+		pending
+	end
+
+	it "puts a group into a hash" do
+		pending
+	end
+
+	it "puts all group hashes into an array" do
+		pending
+	end
+end
